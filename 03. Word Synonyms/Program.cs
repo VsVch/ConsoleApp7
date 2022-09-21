1dc2cb238f0c0cca1649d35f979244b3e6021d72
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-09-21 19:47:32 +0300

    Add project files.

---
+++ 03. Word Synonyms/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var counts = new Dictionary<string, List<string>>();
+
+            string key = string.Empty;
+
+
+
+            for (int i = 1; i <= 2 * n; i++)
+
+            {
+
+                string input = Console.ReadLine();
+
+                if (i % 2 != 0)
+
+                {
+
+                    key = input; // key updates regardless of its presence in counts
+
+
+
+                    if (!(counts.ContainsKey(input)))
+
+                    {
+
+                        counts.Add(input, new List<string>());
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    counts[key].Add(input);
+
+                }
+
+            }
+            foreach (var word in counts)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+
+}
+
